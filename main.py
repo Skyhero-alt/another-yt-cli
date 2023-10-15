4dd808c9e403adf0b22fc012c93862f2429d5b67
Skyhero-alt <skyhero.code@gmail.com>
2023-10-15 22:17:50 +0530

    fix: Fixed relevant search results display

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ from bs4 import BeautifulSoup
 # import time
 
 
-def get_link(name,max_res = 10):
+def get_link(name, max_res=10):
     query = name.split()
     r = requests.get(
         'https://www.youtube.com/results?search_query=' + "+".join(query)).text
 
-    # html = r.content
-    # print('https://www.youtube.com/results?search_query=' + "+".join(name))
-    print("Searching for "+name+"....\n")
+    print("Searching for " + name + "....\n")
 
     soup = BeautifulSoup(r, "lxml")
 
@@ -29,13 +27,6 @@ def get_link(name,max_res = 10):
         "var ytInitialData = (.+)[,;]{1}", str(script)).group(1)
     json_data = json.loads(json_text)
 
-    # ME TESTING :)
-    # with open("file.html", "w", encoding='utf-8') as output:
-    #     output.write(soup.prettify())
-
-    # with open("script.txt", "w", encoding="utf-8") as output:
-    #     output.write(json_text)
-
     content = (
         json_data
         ['contents']['twoColumnSearchResultsRenderer']
@@ -44,7 +35,7 @@ def get_link(name,max_res = 10):
         ['contents']
     )
 
-    links = []
+    links_and_titles = []
     for i in content:
         for key, val in i.items():
             if type(val) is dict:
@@ -52,45 +43,35 @@ def get_link(name,max_res = 10):
                     try:
                         if k == "videoId":
                             video_link = "https://www.youtube.com/watch?v=" + v
-                            links.append(video_link)
-                            # return "https://www.youtube.com/watch?v="+v
-                        # if k =="title":
-                            # print(v["runs"][0]["text"])
+
+                        if k == "title":
+                            video_title = v['runs'][0]['text']
+                            links_and_titles.append([video_link, video_title])
+
                     except Exception as e:
-                        print("An error occured:", e)
-    
-    return links[:max_res]
+                        pass
 
-    # dom = etree.HTML(str(soup))
-    # print(dom.xpath('//*[@id="video-title"]')[0].text)
-    # for i in res:
-    # 	print(i)
-        # print(i.a['id'])
-        # if i.a['id'] == "video-title":
-        # 	print(i)
-
-# get_link()
-
+    return links_and_titles[:max_res]
     
 def selc_video(links):
     """Select the link from list of youtube videos."""
     print("Choose a video to play: ")
+    res = []
     for i, links in enumerate(links,1):
-        print(f"{i}. {links}")
+        print(f"{i}. {links[1]}")
+        res.append(links[0])
+        print()
+
         
     while True:
         try:
             num = int(input("Enter the number of the video link you want to play: "))
-            if 1<=num<=len(links):
-                return links[num-1]
+            if 1<=num<=len(res):
+                return res[num-1]
             else:
                 print("Index out of range.Enter in range of 1 to 10")
         except ValueError:
 	        print("Invalid Input.Enter a numeric digit.")
-        
-# def get_video_name():
-#     name = input("Enter the video name you want to play: ")
-#     return name
 
 
 def play(link):
@@ -99,32 +80,22 @@ def play(link):
         return
 
     print("Trying to play: ", link) 
-    mpv_path = r"C:\\Users\\Viraj Sawant\\Downloads\\bootstrapper\\mpv.exe"
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    try:
-        subprocess.run([mpv_path, "--ytdl-format=18", "--user-agent=" + user_agent, str(link)])
-    except subprocess.CalledProcessError as e:
-        print("Error occurred while playing the video:", e)
+    subprocess.run(["mpv", "--ytdl-format=18", str(link)])
+    
 
     print()
 
 
 # MAIN PROGRAM starts here
 if __name__ == "__main__":
-    # if (len(sys.argv) > 1):
-    #     res = get_link(" ".join(sys.argv[1:]))
-    #     play(res)
-    # else:
-    #     n = get_video_name()
-    #     res = get_link(n)
-    #     play(res)
+  
     if len(sys.argv) > 1:
         url = " ".join(sys.argv[1:])
     else:
         url = input("Enter the video name you want to play: ")
-    links = get_link(url)
-    if not links:
+    vids = get_link(url)
+    if not vids:
         print("No video found.")
     else:
-        sel_link = selc_video(links)
+        sel_link = selc_video(vids)
         play(sel_link)
